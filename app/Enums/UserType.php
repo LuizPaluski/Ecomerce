e0dcf9c3eaf0ba68b408e1933ca5e20bed82003d
LuizPaluski <luizgustavodelimapalauski@gmail.com>
2025-07-15 10:54:09 -0300

    ADD: enuns e mais alguns arquivos que nao estavam no commit anterior

---
+++ app/Enums/UserType.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static user()
+ * @method static static admin()
+ * @method static static moderator()
+ */
+final class UserType extends Enum
+{
+    const USER = 'user';
+    const ADMIN = 'admin';
+    const MODERATOR = 'moderator';
+
+}
